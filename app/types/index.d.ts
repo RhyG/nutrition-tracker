703c081e18d4aaeb3b312c8df63640bf95ca58ee
rhyg <rhysgeary@gmail.com>
2022-11-12 10:11:51 +1000

    Added basic types.

---
+++ app/types/index.d.ts
@@ -0,0 +1,21 @@
+declare namespace NodeJS {
+  interface Global {
+    __DEV__?: boolean;
+    __E2E__: boolean;
+  }
+}
+
+export type ActivityLevel = {
+  label: string;
+  value: string;
+  multiplier: number;
+};
+
+export type JournalEntry = {
+  id: string;
+  name: string;
+  calories: number;
+  protein: number;
+};
+
+export type Day = "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" | "Saturday" | "Sunday";
