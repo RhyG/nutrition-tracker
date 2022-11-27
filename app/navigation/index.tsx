1220e8bb1257eb80053a1962f5651426ccfdd368
rhyg <rhysgeary@gmail.com>
2022-11-27 15:49:23 +1000

    Added (broken/WIP) navigation file.

---
+++ app/navigation/index.tsx
@@ -0,0 +1,56 @@
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import React, { useEffect } from 'react';
+import { StatusBar } from 'react-native';
+import { useTheme } from 'styled-components/native';
+
+import { AboutScreen } from '@features/about';
+import { CalculatorsScreen } from '@features/calculators';
+import { FoodJournalScreen } from '@features/food-journal';
+import { GoalsScreen } from '@features/goals';
+import { WeeklyOverviewScreen } from '@features/weekly-overview';
+
+import { BurgerMenu } from '@app/components/BurgerMenu';
+import { CustomDrawer } from '@app/components/CustomDrawer/CustomDrawer';
+import { HeaderStyle as headerStyle } from '@app/config/themes';
+
+const Drawer = createDrawerNavigator();
+
+const AppNavigator = (): JSX.Element => {
+  const { colours } = useTheme();
+
+  useEffect(() => {
+    StatusBar.setBarStyle('dark-content');
+  }, []);
+
+  return (
+    <Drawer.Navigator
+      initialRouteName="Food Journal"
+      drawerContent={CustomDrawer}
+      useLegacyImplementation={true}
+      screenOptions={{
+        headerStyle,
+        headerLeft: BurgerMenu,
+        headerTitleAlign: 'center',
+        drawerItemStyle: { width: '100%', marginLeft: 0 },
+        drawerActiveBackgroundColor: '#2C3949',
+        drawerLabelStyle: {
+          color: '#fff',
+          fontSize: 16,
+          width: '100%',
+          paddingLeft: 20,
+        },
+        drawerContentStyle: {
+          backgroundColor: colours.darkBlue,
+        },
+        overlayColor: 'transparent',
+      }}>
+      <Drawer.Screen name="Food Journal" component={FoodJournalScreen} />
+      <Drawer.Screen name="Weekly Overview" component={WeeklyOverviewScreen} />
+      <Drawer.Screen name="Goals" component={GoalsScreen} />
+      <Drawer.Screen name="Calculators" component={CalculatorsScreen} />
+      <Drawer.Screen name="About" component={AboutScreen} />
+    </Drawer.Navigator>
+  );
+};
+
+export default AppNavigator;
