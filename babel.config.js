f1e03f29e8e28791a8a64fea160853b922f4d1a0
rhyg <rhysgeary@gmail.com>
2022-11-30 21:26:16 +1000

    Various config and dep updates.

--- babel.config.js
+++ babel.config.js
@@ -1,26 +1,27 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
-      "react-native-reanimated/plugin",
+      'react-native-reanimated/plugin',
       [
-        "module-resolver",
+        'module-resolver',
         {
           alias: {
-            "@app": path.resolve(__dirname, "./app"),
-            "@config": path.resolve(__dirname, "./app/config"),
-            "@e2e": path.resolve(__dirname, "./e2e"),
-            "@components": path.resolve(__dirname, "./app/components"),
-            "@feature": path.resolve(__dirname, "./app/screens"),
-            "@food-journal": path.resolve(__dirname, "./app/screens/food-journal"),
-            "@goals": path.resolve(__dirname, "./app/screens/goals"),
-            "@about": path.resolve(__dirname, "./app/screens/about"),
-            "@calculators": path.resolve(__dirname, "./app/screens/calculators"),
+            '@app': path.resolve(__dirname, './app'),
+            '@config': path.resolve(__dirname, './app/config'),
+            '@theme': path.resolve(__dirname, './app/theme'),
+            '@e2e': path.resolve(__dirname, './e2e'),
+            '@components': path.resolve(__dirname, './app/components'),
+            '@feature': path.resolve(__dirname, './app/screens'),
+            '@food-journal': path.resolve(__dirname, './app/screens/food-journal'),
+            '@goals': path.resolve(__dirname, './app/screens/goals'),
+            '@about': path.resolve(__dirname, './app/screens/about'),
+            '@calculators': path.resolve(__dirname, './app/screens/calculators'),
           },
-          cwd: "babelrc",
-          root: ["./app", "./jest"],
-          extensions: [".e2e.ts", ".e2e.tsx", ".d.ts", ".ts", ".tsx", ".js", ".ios.js", ".android.js"],
+          cwd: 'babelrc',
+          root: ['./app'],
+          extensions: ['.e2e.ts', '.e2e.tsx', '.d.ts', '.ts', '.tsx', '.js', '.ios.js', '.android.js'],
         },
       ],
     ],
