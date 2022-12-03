e396b87a604b43ed023c56a8752f33947d816b13
rhyg <rhysgeary@gmail.com>
2022-12-04 08:34:42 +1000

    FINALLY got module resolution working - updating imports as I go.

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = function (api) {
-  if (api) api.cache(true);
+  api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
@@ -7,22 +7,15 @@ module.exports = function (api) {
       [
         'module-resolver',
         {
-          root: ['./app'],
+          root: ['./'],
           alias: {
-            '@app': path.resolve(__dirname, './app'),
-            // '@config': path.resolve(__dirname, './app/config'),
-            // '@theme': path.resolve(__dirname, './app/theme'),
-            // '@screens': path.resolve(__dirname, './app/screens'),
-            // '@components': path.resolve(__dirname, './app/components'),
-            // '@e2e': path.resolve(__dirname, './e2e'),
-            // '@feature': path.resolve(__dirname, './app/screens'),
-            // '@food-journal': path.resolve(__dirname, './app/screens/food-journal'),
-            // '@goals': path.resolve(__dirname, './app/screens/goals'),
-            // '@about': path.resolve(__dirname, './app/screens/about'),
-            // '@calculators': path.resolve(__dirname, './app/screens/calculators'),
+            test: './test',
+            '@theme': './app/theme',
+            '@app': './app',
+            '@components': './app/components',
+            '@hooks': './app/hooks',
+            '@config': './app/config',
           },
-          cwd: 'babelrc',
-          extensions: ['.js', '.jsx', '.ts', '.tsx', '.android.js', '.android.tsx', '.ios.js', '.ios.tsx'],
         },
       ],
     ],
