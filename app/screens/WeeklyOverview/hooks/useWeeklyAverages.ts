388e052272655b0aecc8877de22dbf5aa366273e
rhyg <rhysgeary@gmail.com>
2022-12-04 11:44:03 +1000

    Added useWeeklyAverages hook.

---
+++ app/screens/WeeklyOverview/hooks/useWeeklyAverages.ts
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+
+import { JournalData, useJournal } from '@app/store/journal';
+import { Day, JournalEntry } from '@app/types';
+
+type ReducedJournalEntry = Pick<JournalEntry, 'protein' | 'calories'>;
+
+const DEFAULT_AVERAGES: ReducedJournalEntry = {
+  calories: 0,
+  protein: 0,
+};
+
+const getAverages = (data: JournalData) => {
+  const dailyTotals = Object.keys(data).map((key: string) => {
+    const dailyTotal = data[key as Day].reduce(
+      (acc: ReducedJournalEntry, curr: ReducedJournalEntry) => ({
+        calories: Number(acc.calories) + Number(curr.calories),
+        protein: Number(acc.protein) + Number(curr.protein),
+      }),
+      DEFAULT_AVERAGES,
+    );
+
+    return dailyTotal;
+  });
+
+  const calories = Math.round(
+    dailyTotals.reduce((a, c) => a + c.calories, 0) / 7,
+  );
+  const protein = Math.round(
+    dailyTotals.reduce((a, c) => a + c.protein, 0) / 7,
+  );
+
+  return { calories, protein };
+};
+
+export const useWeeklyAverages = () => {
+  const [averages, setAverages] = useState(DEFAULT_AVERAGES);
+  const journalData = useJournal(state => state.journalData);
+
+  useEffect(() => {
+    setAverages(getAverages(journalData));
+  }, [journalData]);
+
+  return {
+    averageCalories: averages.calories,
+    averageProtein: averages.protein,
+  };
+};
