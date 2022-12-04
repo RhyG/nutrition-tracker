a9fc623191f3ae1952b086b141c8ad571b41c0b5
rhyg <rhysgeary@gmail.com>
2022-12-05 09:20:11 +1000

    Hydrate app with stored goal and journal data at start.

--- App.tsx
+++ App.tsx
@@ -1,9 +1,24 @@
 import 'react-native-gesture-handler';
+import 'react-native-get-random-values';
 import { NavigationContainer } from '@react-navigation/native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { MenuProvider } from 'react-native-popup-menu';
 
 import AppNavigator from './app/navigation';
+import { Goals, DefaultGoals, useGoals } from '@app/store/goals';
+import { DefaultJournalData, useJournal } from '@app/store/journal';
+import AsyncStorage from '@app/modules/AsyncStorage';
+
+// Fetch goals and current data from storage on app mount
+(async () => {
+  const storedGoals = await AsyncStorage.getItem<Goals>('goals', DefaultGoals);
+  useGoals.getState().updateGoals(storedGoals);
+
+  const storedData = await AsyncStorage.getItem<{
+    [key in keyof typeof DefaultJournalData]: [];
+  }>('journalData', DefaultJournalData);
+  useJournal.getState().updateJournal(storedData);
+})();
 
 export default function App() {
   return (
