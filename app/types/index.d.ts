42410c3d0c0efcd75c8326feda3408c40c486b7d
rhyg <rhysgeary@gmail.com>
2023-10-15 16:15:44 +1000

    Okay now fixed all type errors.

--- app/types/index.d.ts
+++ app/types/index.d.ts
@@ -5,9 +5,12 @@ declare namespace NodeJS {
   }
 }
 
+// TODO: Remove all project specific types from here.
 export type JournalEntry = {
   id: string;
   name: string;
   calories: number;
   protein: number;
 };
+
+export type Day = 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday' | 'Sunday';
