fe64de26de920be85d53e88f1b63e1e9196563f3
rhyg <rhysgeary@gmail.com>
2023-11-01 20:53:12 +1000

    i18n setup.

---
+++ app/i18n/en.ts
@@ -0,0 +1,11 @@
+export const en = {
+  macros: {
+    calories: 'Calories',
+    carbohydrates: 'Carbohydrates',
+    carbs: 'Carbs',
+    protein: 'Protein',
+    fat: 'Fat',
+  },
+} as const;
+
+export type EnglishTxns = typeof en;
