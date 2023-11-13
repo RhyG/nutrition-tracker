2f4a2740922abcf4fecbda680c482f04801e03ea
rhyg <rhysgeary@gmail.com>
2023-11-13 14:59:03 +1000

    Util improvements.

--- app/lib/populate-journal.ts
+++ app/lib/populate-journal.ts
@@ -1,6 +1,7 @@
 import { nanoid } from 'nanoid';
 
 import { useDayStore } from '@app/store/day';
+import { useGoalsStore } from '@app/store/goals';
 import { useFoodLogStore } from '@app/store/journal';
 import { Day, FoodLogEntry } from '@app/types';
 
@@ -76,11 +77,51 @@ export function generateEntries(amount = 20): FoodLogEntry[] {
 }
 
 export function fillDay(day?: Day) {
-  const entries = generateEntries();
+  // const entries = generateEntries();
+  const { calories, protein, carbohydrates, fat } = useGoalsStore.getState();
+  const entries = generate(calories, protein, carbohydrates, fat);
   useFoodLogStore.getState().fillDay(entries, day ?? useDayStore.getState().currentDay);
 }
 
 export function fillWeek() {
   const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] as Day[];
   days.forEach(day => fillDay(day));
+}
+
+function generate(calories: number, protein: number, carbohydrates: number, fat: number) {
+  const numObjects = Math.floor(Math.random() * 8) + 5; // Generate between 5 and 12 objects
+  const entries: FoodLogEntry[] = [];
+
+  if (!__DEV__) {
+    return entries;
+  }
+
+  for (let i = 0; i < numObjects; i++) {
+    const name = FOODS[generateRandomNumber(0, FOODS.length)] ?? '';
+    entries.push({ name, id: nanoid(), calories: 0, protein: 0, carbohydrates: 0, fat: 0, timestamp: getRandomEpochTime() });
+  }
+
+  const distribute = (limit: number, property: string) => {
+    let remaining = limit;
+
+    for (let i = 0; i < entries.length; i++) {
+      if (i === entries.length - 1) {
+        // @ts-expect-error -- can't be assed it's a dev mode util
+        entries[i][property] = remaining;
+      } else {
+        let value = Math.random() * remaining;
+        value = Math.min(value, remaining - (entries.length - i - 1));
+        // @ts-expect-error -- can't be assed it's a dev mode util
+        entries[i][property] = Math.round(value);
+        remaining -= value;
+      }
+    }
+  };
+
+  distribute(calories, 'calories');
+  distribute(protein, 'protein');
+  distribute(carbohydrates, 'carbohydrates');
+  distribute(fat, 'fat');
+
+  return entries;
 }
